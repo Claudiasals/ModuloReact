4e5d01e5b54e8ba17afdcca6ba9dabd3dfe6d5b8
Claudiasals <salsiniclaudia@gmail.com>
2025-09-19 00:36:13 +0200

    Es. 4: Crea un componente classe “Hello World” di rendering all’interno dell’App

---
+++ AppReact/src/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react"
+
+claass App extends Component{
+    render () {
+        return (
+            <>
+            <p>Hello World!!</p>
+            </>
+        )
+    }
+}
+export default App;
